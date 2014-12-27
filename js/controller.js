06dd52b4335ba0dda5cdd7f403673afbc7e9e53d
andela-amaborukoje <adebayo@andela.co>
2014-12-27 17:53:31 +0100

    converted to json format

--- js/controller.js
+++ js/controller.js
@@ -4,8 +4,11 @@ portfolio.controller('portfolioCtrl', [ '$scope', function($scope){
 
 }]);
 
-portfolio.controller('homeCtrl', [ '$scope', function($scope){
-  
+portfolio.controller('homeCtrl', [ '$scope', '$http', function($scope, $http){
+  $http.get('./projects/projects.json').success(function(data){
+    $scope.projects = data;
+console.log($scope.projects);
+  });
 }]);
 
 portfolio.controller('contactCtrl', [ '$scope', function($scope){
