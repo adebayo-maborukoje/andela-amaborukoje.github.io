baf45544147135b813d73964dfbaf27c27d9f41e
andela-amaborukoje <adebayo@andela.co>
2014-12-31 12:08:50 +0100

    completed the protfolio page

--- js/controller.js
+++ js/controller.js
@@ -1,13 +1,19 @@
 var portfolio = angular.module('portfolio' );
 
-portfolio.controller('portfolioCtrl', [ '$scope', function($scope){
-
+portfolio.controller('portfolioCtrl', [ '$scope', '$http', function($scope, $http){
+		$http.get('./projects/projects.json').success(function(data){
+		$scope.projects = data;
+		//	console.log($scope.projects);
+		}).error( function(err, status){
+			$scope.projects.name = err;	
+		});
 }]);
 
 portfolio.controller('homeCtrl', [ '$scope', '$http', function($scope, $http){
   $http.get('./projects/projects.json').success(function(data){
-    $scope.projects = data;
-console.log($scope.projects);
+    $scope.projects = data;	
+	console.log($scope.projects);
+	$scope.order = '-age';
   });
 }]);
 
